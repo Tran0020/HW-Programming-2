555f873f778a854017ee2dded16a85b1e1aeff9e
Jenna Tran <Jenna.h.tran@ou.edu>
2019-09-02 21:40:57 -0500

    commit array 1

---
+++ HW 1 Progamming 2 Jenna Tran/Array1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Array1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a console application that prompts the user to enter the item they desire, and return the value / price of the item.
+            //  Your console application should contain two parallel arrays, as well as an error if they enter or spell 
+            //   the item name incorrectly.
+
+            //Array #1: should contain items apples, oranges, bananas, grapes, blueberries
+            //Array #2: should contain prices for each item.
+            //Apples are 0.99, oranges are 0.50, bananas are 0.50, grapes are 2.99, blueberries are 1.99.
+
+            string[] fruit = { "apples", "oranges", "bananas", "grapes", "blueberries" };
+            double[] price = { 0.99, 0.50, 0.50, 2.99, 1.99 };
+            string fruitname;
+            bool isvalid = false;
+            Console.WriteLine("Please enter the fruit you desire.");
+            fruitname = Console.ReadLine();
+            int i;
+           do
+            {
+                for (i = 0; i < fruit.Length - 1; i++)
+                {
+                    if (fruitname == fruit[i])
+                    {
+                        Console.WriteLine($"The price of {fruitname} is {price[i]:c}");
+                    isvalid = true;
+                    }
+
+                }
+                if (isvalid == false)
+                {
+                    Console.WriteLine("Error! Please enter the fruit name again.");
+                    fruitname = Console.ReadLine();
+
+                }
+
+            } while (isvalid==false);
+
+
+            Console.ReadLine();
+            }   
+    }
+}
--- HW 1 Progamming 2 Jenna Tran/Methods/Program.cs
+++ HW 1 Progamming 2 Jenna Tran/Methods/Program.cs
@@ -22,22 +22,32 @@ namespace Methods
 
         }
 
-//        The second method that you will need to create is to calculate the factorial of a number.
-//            Your method name should be Factorial and should have a return type of int.  
-//            Your method should accept a single parameter of type int.  
-//            To calculate the factorial of a number, you multiply it by every number before it.
-//e.g. 5 factorial = 5 * 4 * 3 * 2 * 1 = 120
+        //        The second method that you will need to create is to calculate the factorial of a number.
+        //            Your method name should be Factorial and should have a return type of int.  
+        //            Your method should accept a single parameter of type int.  
+        //            To calculate the factorial of a number, you multiply it by every number before it.
+        //e.g. 5 factorial = 5 * 4 * 3 * 2 * 1 = 120
 
-        //    static int Factorial(int num)
-        //{
+        static int Factorial(int num)
+        {
+            int factor = 1;
+            for(int i=1;i<=num; i++)
+            {
+             
+                factor=factor*i;
+               
 
-        //}
+            }
+            return factor;
+        }
 
         static void Main(string[] args)
         {
-            double slope = LineValueForY(1, 2, 3);
-            Console.WriteLine($"The slope is {slope}");
+            ////double slope = LineValueForY(1, 2, 3);
+            ////Console.WriteLine($"The slope is {slope}");
 
+            int factor = Factorial(5);
+            Console.WriteLine(factor);
 
 
         }
